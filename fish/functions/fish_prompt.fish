4f89ff3efcfa97d8eacf82b0ff4e39db59c602aa
Oberdorf Urs <u.oberdorf@eurodata.de>
2018-06-18 08:30:22 +0200

    added git branch

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,6 +3,9 @@ function fish_prompt
 
     test $USER = 'root'; and echo (set_color red)"#"
 
+    # set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
+    set -l git_branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\[\1\] /')
+
     # Main
-	echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+	echo -n (set_color cyan)(prompt_pwd) (set_color green)"$git_branch"(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end
